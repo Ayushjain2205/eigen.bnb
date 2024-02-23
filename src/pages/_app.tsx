0e1609a8037c73ec2b213a1d2517bf32a90335bd
Ayush Jain <ayushjain2205@gmail.com>
2024-02-23 20:39:13 +0530

    Setup thirdweb

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { BinanceTestnet } from "@thirdweb-dev/chains";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThirdwebProvider
+      activeChain={BinanceTestnet}
+      clientId="39a24e614ae8f21dec3aef65eb2fc094"
+    >
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
